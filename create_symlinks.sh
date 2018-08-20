f17e6cfe51c43ceb61184f1d2f539bcbb251ca97
JulienPascal <julien.pascal@sciencespo.fr>
2018-08-20 11:53:50 +0200

    changes

---
+++ create_symlinks.sh
@@ -0,0 +1,13 @@
+echo "+++++++++++++++++++++"
+echo "Creating Symlinks for:"
+echo "LiseRobin2016"
+echo "LisePascalRobin"
+echo "Palvit"
+echo "Reiter2009"
+echo "Robin2011"
+echo "+++++++++++++++++++++"
+ln -s ~/.julia/v0.6/LiseRobin2016 ~/Documents/REPOSITORIES/LiseRobin2016
+ln -s ~/.julia/v0.6/LisePascalRobin ~/Documents/REPOSITORIES/LisePascalRobin
+ln -s ~/.julia/v0.6/Palvit ~/Documents/REPOSITORIES/Palvit
+ln -s ~/.julia/v0.6/Reiter2009 ~/Documents/REPOSITORIES/Reiter2009
+ln -s ~/.julia/v0.6/Robin2011 ~/Documents/REPOSITORIES/Robin2011
--- install_julia_unregistered_packages.sh
+++ install_julia_unregistered_packages.sh
@@ -16,17 +16,3 @@ Pkg.clone("https://github.com/mrxiaohe/RobustStats.jl");
 #Pkg.clone("https://github.com/JulienPascal/BlsData.jl") #the original has a deprecated REQUIRE file
 
 
-echo "+++++++++++++++++++++"
-echo "Creating Symlinks for:"
-echo "LiseRobin2016"
-echo "LisePascalRobin"
-echo "Palvit"
-echo "Reiter2009"
-echo "Robin2011"
-echo "+++++++++++++++++++++"
-ln -s ~/.julia/v0.6/LiseRobin2016 ~/Documents/REPOSITORIES/LiseRobin2016
-ln -s ~/.julia/v0.6/LisePascalRobin ~/Documents/REPOSITORIES/LisePascalRobin
-ln -s ~/.julia/v0.6/Palvit ~/Documents/REPOSITORIES/Palvit
-ln -s ~/.julia/v0.6/Reiter2009 ~/Documents/REPOSITORIES/Reiter2009
-ln -s ~/.julia/v0.6/Robin2011 ~/Documents/REPOSITORIES/Robin2011
-
