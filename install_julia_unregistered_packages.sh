dfc79e285d333ff073d04df71a0dc376570aa197
JulienPascal <julien.pascal@sciencespo.fr>
2018-08-20 11:46:15 +0200

    changes

--- install_julia_unregistered_packages.sh
+++ install_julia_unregistered_packages.sh
@@ -1,14 +1,19 @@
+# Within Julia
+#------------
 Pkg.clone("https://github.com/floswald/ApproXD.jl");
 Pkg.clone("https://github.com/floswald/Copulas.jl");
 Pkg.clone("https://github.com/floswald/MomentOpt.jl");
 Pkg.clone("https://github.com/RJDennis/SmolyakApprox.jl");
 Pkg.clone("https://github.com/mrxiaohe/RobustStats.jl");
+
 # Within Julia
+#------------
 #Pkg.clone("https://github.com/JulienPascal/Palvit.git")
 #Pkg.clone("https://github.com/JulienPascal/LiseRobin2016.jl.git")
 #Pkg.clone("https://github.com/JulienPascal/LisePascalRobin.jl.git")
 #Pkg.clone("https://github.com/JulienPascal/Reiter2009.jl.git")
 #Pkg.clone("https://github.com/JulienPascal/Robin2011.jl.git")
+#Pkg.clone("https://github.com/JulienPascal/BlsData.jl") #the original has a deprecated REQUIRE file
 
 
 echo "+++++++++++++++++++++"
