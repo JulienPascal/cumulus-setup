005d9b2414d5f587748157180ef557aacd914981
JulienPascal <julien.pascal@sciencespo.fr>
2018-11-12 16:31:55 +0000

    installing anaconda

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -1,2 +1,6 @@
-https://repo.continuum.io/archive/Anaconda3-5.3.O-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 bash Anaconda3-5.3.0-Linux-x86_64.sh
+rm Anaconda3-5.3.0-Linux-x86_64.sh
+echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashr
+source .bashrc
+conda update conda
